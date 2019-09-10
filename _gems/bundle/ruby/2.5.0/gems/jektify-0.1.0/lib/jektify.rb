ca48b63fca56e86b44f75a1a074034ea532e3f19
xkjskk@163.com <xkjskk@163.com>
2019-09-10 11:33:24 +0800

    更换主题

--- _gems/bundle/ruby/2.5.0/gems/jektify-0.1.0/lib/jektify.rb
+++
@@ -1,9 +0,0 @@
-require "jektify/version"
-
-module Jektify
-  class Debut
-    def say!
-      p "Jektify Soon you will be circulating!"
-    end
-  end
-end
--- _gems/bundle/ruby/2.5.0/gems/jektify-0.1.0/lib/jektify/version.rb
+++
@@ -1,3 +0,0 @@
-module Jektify
-  VERSION = "0.1.0"
-end
